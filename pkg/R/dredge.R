2514a5374a0de3d99c29d2f36709d6c9edefd7d6
prezez (Kamil Bartoń) <kamil.barton@o2.pl>
2024-06-08 08:21:41 +0000

    modified methods for unmarkedFit (new umf_specs)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@524 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dredge.R
+++ pkg/R/dredge.R
@@ -193,12 +193,9 @@ function(global.model, beta = c("none", "sd", "partial.sd"),
 			fixed <- fixed[i]
 		}
 	}
-
-	#deps <- attr(allTerms0, "deps")
 	fixed <- union(fixed, rownames(deps)[rowSums(deps, na.rm = TRUE) == ncol(deps)])
 	fixed <- c(fixed, allTerms[allTerms %in% interceptLabel])
     fixed <- fixed[!duplicated(fixed)]
-
 	nFixed <- length(fixed)
 	if(nFixed != 0L) message(sprintf(ngettext(nFixed, "Fixed term is %s", "Fixed terms are %s"),
 		prettyEnumStr(fixed)))
